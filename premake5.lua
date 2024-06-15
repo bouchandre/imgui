d8ceaf582355bc8d984b1d0a6e7c4224d53927fa
Alex <69326200+bouchandre@users.noreply.github.com>
2024-06-14 20:27:58 -0400

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -3,8 +3,8 @@ project "ImGui"
 	language "C++"
     staticruntime "off"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir (staticLibTargetDir)
+	objdir (staticLibObjDir)
 
 	files
 	{
